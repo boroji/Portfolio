8b93ce0d47d3815cb4c385e318fbc7962b5d9258
Hossein <hosseinboroji@gmail.com>
2020-02-29 22:48:54 -0500

    i finally made it

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var seekProgressEl = document.querySelector('.progress');
+/* var seekProgressEl = document.querySelector('.progress');
 
 var animation = anime({
     targets: '.line-drawing',
@@ -22,4 +22,42 @@ seekProgressEl.oninput = function () {
 animation.play();
 animation.pause();
 animation.restart();
-animation.seek(timeStamp);
+animation.seek(timeStamp); */
+
+var controlsProgressEl = document.querySelector('.progress');
+
+var seekProgressEl = document.querySelector('.progress');
+
+var tl = anime.timeline({
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function (el, i) {
+            return i * 10
+        },
+        direction: 'alternate',
+        loop: true,
+        autoplay: true
+});
+
+tl
+.add({
+    targets: '.line-drawing',
+    strokeDashoffset: [anime.setDashoffset, 0],
+})
+.add({
+    targets: '.shape',
+    scale: [0, 1],
+}, '-=100');
+
+document.querySelector('.play').onclick = tl.play;
+document.querySelector('.pause').onclick = tl.pause;
+document.querySelector('.restart').onclick = tl.restart;
+
+controlsProgressEl.addEventListener('input', function () {
+    tl.seek(tl.duration * (controlsProgressEl.value / 100));
+});
+
+timeline.play();
+timeline.pause();
+timeline.restart();
+timeline.seek(timeStamp);
